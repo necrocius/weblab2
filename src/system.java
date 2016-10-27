c36d78cfaec6f503ebf7eee8931e751ed31d9a00
necrocius <100ishundred@gmail.com>
2016-10-27 18:28:04 +0500

    Создан классы system и systemTest

---
+++ src/system.java
@@ -0,0 +1,11 @@
+public class system {
+    float a, b, c;
+    system(float a, float b, float c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    String decision(system ob1) {
+        return "9";
+    }
+}
---
+++ test/systemTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author necrocius
+ */
+public class systemTest {
+    
+    public systemTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void hello() {
+        system s1 = new system(1, 2, 3);
+        system s2 = new system(4, 5, 6);
+        assertEquals(s1.decision(s2),"9");
+    }
+}
